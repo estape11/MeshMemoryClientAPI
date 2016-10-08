124006ae2e4108af8140fd7862bd1675a12aa293
estape11 <thekillers11>
2016-10-07 19:44:06 -0600

    Fix something

--- main.cpp
+++ main.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main(){
     API memoria=API();
-    string token=memoria.initialize("172.26.98.253", 8080);
-    cout<<token<<endl;
+    cout<<"Iniciando conexion"<<endl;
+    string token=memoria.initialize("172.26.109.10", 8080);
+    cout<<"Token: "<<token<<endl;
+    cout<<"xMalloc de 4 bytes"<<endl;
     xReference referencia= memoria.xMalloc(4,xType::INTEGER);
     int numero=44;
     memoria.xAssign(referencia, &numero);
-
     cout<<*(int*)(memoria.xDereference(referencia))<<endl;
+    cout<<"xMalloc de 1 bytes"<<endl;
     xReference referencia2= memoria.xMalloc(1,xType::CHAR);
-
     memoria.xFree(referencia);
-    //char hola='h';
-    //memoria.xAssign(referencia2, &hola);
-
-    //char valor=*(char*)(memoria.xDereference(referencia2));
-
-    //cout<<valor<<endl;
-
+    cout<<"xMalloc de 2 bytes"<<endl;
+    xReference referencia3=memoria.xMalloc(2,xType::INTEGER);
+    int num=544;
+    memoria.xAssign(referencia3,&num);
+    cout<<*(int*)(memoria.xDereference(referencia3))<<endl;
+    memoria.xFree(referencia2);
     cout<<"Exito..."<<endl;
 
 }
